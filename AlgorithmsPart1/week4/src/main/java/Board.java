c8c833ff6549bf7f3267bf8b6dff6e2c1ddcd44d
nicusorb@gmail.com <nicolae@123>
2014-03-04 16:54:13 +0200

    Coursera - Algorithms part 1 - week 4 8-puzzle

--- AlgorithmsPart1/week4/src/main/java/Board.java
+++ AlgorithmsPart1/week4/src/main/java/Board.java
@@ -1,25 +1,85 @@
+
 public class Board {
+    private Board previous;
     private int[][] blocks;
+    private int moves;
 
     // construct a board from an N-by-N array of blocks (where blocks[i][j] = block in row i, column j)
     public Board(int[][] blocks) {
-        this.blocks = blocks;
+        this(null, blocks, 0);
     }
 
-    // board dimension N
+    public Board(Board previous, int[][] blocks, int moves) {
+        this.previous = previous;
+        this.blocks = blocks;
+        this.moves = moves;
+    }
+
+    // board dimension dimension
     public int dimension() {
         return blocks.length;
     }
 
     // number of blocks out of place
     public int hamming() {
+        int hamming = 0;
+        for (int i = 0; i < dimension(); i++) {
+            for (int j = 0; j < dimension(); j++) {
+                if (!isInRightPlace(i, j)) {
+                    hamming += 1;
+                }
+            }
+        }
 
+        return hamming + moves;
     }
 
-//    public int manhattan()                 // sum of Manhattan distances between blocks and goal
-//    public boolean isGoal()                // is this board the goal board?
-//    public Board twin()                    // a board obtained by exchanging two adjacent blocks in the same row
+    // sum of Manhattan distances between blocks and goal
+//    public int manhattan() {
+//        //TODO
+//        int manhattan = 0;
+//
+//        return manhattan + moves;
+//    }
+
+    // is this board the goal board?
+    public boolean isGoal() {
+        for (int i = 0; i < blocks.length; i++) {
+            for (int j = 0; j < blocks[i].length; j++) {
+                if (!isInRightPlace(i, j))
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    //    public Board twin()                    // a board obtained by exchanging two adjacent blocks in the same row
+
+    // string representation of the board (in the output format specified below)
+    public String toString() {
+        String out = "";
+        out += moves + "\n";
+        for (int i = 0; i < blocks.length; i++) {
+            for (int j = 0; j < blocks[i].length; j++) {
+                if (j < blocks[i].length - 1) {
+                    out += blocks[i][j] + " ";
+                } else {
+                    out += blocks[i][j];
+                }
+            }
+            out += "\n";
+        }
+
+        return out;
+    }
+
+    // all neighboring boards
+    public Iterable<Board> neighbors() {
+        return null;
+    }
 //    public boolean equals(Object y)        // does this board equal y?
-//    public Iterable<Board> neighbors()     // all neighboring boards
-//    public String toString()               // string representation of the board (in the output format specified below)
+
+    private boolean isInRightPlace(int i, int j) {
+        return blocks[i][j] == 0 || blocks[i][j] == (dimension() * i + j + 1);
+    }
 }
---
+++ AlgorithmsPart1/week4/src/test/java/BoardTest.java
@@ -0,0 +1,39 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class BoardTest {
+    @Test
+    public void hamming_tests() throws Exception {
+        int[][] blocks = {{8, 1, 3}, {4, 0, 2}, {7, 6, 5}};
+        Board board = new Board(blocks);
+
+        assertThat(board.hamming(), is(5));
+    }
+
+    @Test
+    public void isGoal_returnsTrue_ifItIsTheFinalBoard() throws Exception {
+        int[][] blocks = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
+        Board board = new Board(blocks);
+
+        assertThat(board.isGoal(), is(true));
+    }
+
+    @Test
+    public void isGoal_returnsFalse_ifItIsNotTheFinalBoard() throws Exception {
+        int[][] blocks = {{1, 2, 3}, {4, 6, 5}, {7, 8, 0}};
+        Board board = new Board(blocks);
+
+        assertThat(board.isGoal(), is(false));
+    }
+
+    @Test
+    public void toString_test() throws Exception {
+        int[][] blocks = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
+        Board board = new Board(blocks);
+
+        assertThat(board.toString(), is("0\n1 2 3\n4 5 6\n7 8 0\n"));
+
+    }
+}
