89e19548fd5b9706bcc09778200ff886a0d753e7
nicusorb@gmail.com <nicolae@123>
2015-01-30 17:50:28 +0200

    Algorithms part1

--- AlgorithmsPart1/week1/src/main/java/Percolation.java
+++ AlgorithmsPart1/week1/src/main/java/Percolation.java
@@ -1,25 +1,54 @@
 public class Percolation {
+    private int nbOfSites;
+    private boolean[][] sitesStatus;
+    private WeightedQuickUnionUF weightedQuickUnionUF;
+
     // create N-by-N grid, with all sites blocked
     public Percolation(int N) {
+        if (N <= 0)
+            throw new IndexOutOfBoundsException();
+        nbOfSites = N;
+        weightedQuickUnionUF = new WeightedQuickUnionUF(nbOfSites * nbOfSites);
+        sitesStatus = new boolean[nbOfSites][nbOfSites];
+        for (int i = 0; i < nbOfSites; i++) {
+            for (int j = 0; j < nbOfSites; i++) {
+                sitesStatus[i][j] = false;
+            }
+        }
     }
 
     // open site (row i, column j) if it is not open already
     public void open(int i, int j) {
+        weightedQuickUnionUF.union(i, j);
+        sitesStatus[i - 1][j - 1] = true;
     }
 
     // is site (row i, column j) open?
     public boolean isOpen(int i, int j) {
-        return false;
+        return siteStatus(i, j);
     }
 
     // is site (row i, column j) full?
     public boolean isFull(int i, int j) {
-        return false;
+        return siteStatus(i, j);
     }
 
     // does the system percolate?
     public boolean percolates() {
+        for (int i = 1; i <= nbOfSites; i++) {
+            if (isOpen(1, i)) {
+                for (int j = 1; j <= nbOfSites; j++) {
+                    if (isOpen(nbOfSites, j)) {
+//                        if (weightedQuickUnionUF.connected())
+                    }
+                }
+            }
+        }
         return false;
+    }
+
+    private boolean siteStatus(int i, int j) {
+        return sitesStatus[i - 1][j - 1];
     }
 
     // test client (optional)
--- AlgorithmsPart1/week1/src/test/java/PercolationTest.java
+++ AlgorithmsPart1/week1/src/test/java/PercolationTest.java
@@ -1,10 +1,29 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class PercolationTest {
-    @Test
-    public void testName() throws Exception {
 
+    private Percolation sut;
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void throwExceptionIfNumberOfSitesIsNotPositive() throws Exception {
+        Percolation sut = new Percolation(0);
     }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void openShouldTrowExceptionIfRowOutOfBoundaries() throws Exception {
+        sut = new Percolation(4);
+
+        sut.open(5, 2);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void openShouldTrowExceptionIfColumnOutOfBoundaries() throws Exception {
+        sut = new Percolation(4);
+
+        sut.open(2, 5);
+    }
+
 }
