6073401106493b1b6f072b902d5c4a5f8dc65000
nicusorb@gmail.com <nicolae@123>
2015-03-06 10:26:58 +0200

    Implemented PointSet

--- AlgorithmsPart1/week5/src/main/java/PointSET.java
+++ AlgorithmsPart1/week5/src/main/java/PointSET.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class PointSET {
     private SET<Point2D> point2DSET;
 
@@ -35,12 +38,27 @@ public class PointSET {
 
     // all points that are inside the rectangle
     public Iterable<Point2D> range(RectHV rect) {
-        return null;
+        List<Point2D> pointsInRectangle = new LinkedList<>();
+        for (Point2D p : point2DSET) {
+            if (rect.contains(p))
+                pointsInRectangle.add(p);
+        }
+        return pointsInRectangle;
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
-        return new Point2D(1, 1);
+        Point2D nearestPoint = null;
+        double minDistance = Double.MAX_VALUE;
+        for (Point2D point2D : point2DSET) {
+            double distance = p.distanceTo(point2D);
+            if (distance < minDistance) {
+                minDistance = distance;
+                nearestPoint = point2D;
+            }
+        }
+
+        return nearestPoint;
     }
 
     // unit testing of the methods (optional)
--- AlgorithmsPart1/week5/src/test/java/PointSETTest.java
+++ AlgorithmsPart1/week5/src/test/java/PointSETTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 
 public class PointSETTest {
@@ -33,5 +34,19 @@ public class PointSETTest {
         sut.insert(new Point2D(1, 1));
 
         assertThat(sut.size(), is(1));
+    }
+
+    @Test
+    public void nearestPoint_returnsNullIfThereIsNoPoint() throws Exception {
+        assertNull(sut.nearest(new Point2D(1, 1)));
+    }
+
+    @Test
+    public void nearestPoint_returnsTheCorectPoint() throws Exception {
+        sut.insert(new Point2D(1, 1));
+
+        Point2D nearest = sut.nearest(new Point2D(1, 1));
+        assertThat(nearest.x(), is(1.0));
+        assertThat(nearest.y(), is(1.0));
     }
 }
