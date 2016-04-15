8b4ea25a61eab220aa64119244af1c303ccc78fa
Nicu Bucalaete <nicusorb@gmail.com>
2016-04-15 17:13:00 +0300

    SeamCarving improved version

--- algorithms-part2/week2-SeamCarving/src/main/java/SeamCarver.java
+++ algorithms-part2/week2-SeamCarving/src/main/java/SeamCarver.java
@@ -1,16 +1,19 @@
 import edu.princeton.cs.algs4.Picture;
-import edu.princeton.cs.algs4.Stack;
 
-import java.awt.*;
+import java.awt.Color;
 
 public class SeamCarver {
     private Picture picture;
+    private double[][] energy;
+    private boolean transposed;
+    private double[][] energyTransposed;
 
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
         if (picture == null)
             throw new NullPointerException();
         this.picture = picture;
+        computeEnergyMatrix();
     }
 
     // current picture
@@ -42,44 +45,54 @@ public class SeamCarver {
 
     // sequence of indices for horizontal seam
     public int[] findHorizontalSeam() {
-        return null;
+        double[][] distTo = new double[height()][width()];
+        int[][] edgeTo = new int[height()][width()];
+
+
+        return findVerticalSeam(distTo, edgeTo, energyTransposed);
     }
 
     // sequence of indices for vertical seam
     public int[] findVerticalSeam() {
-        double[][] energy = new double[width()][height()];
-        for (int i = 0; i < energy.length; i++) {
-            for (int j = 0; j < energy[i].length; j++) {
-                energy[i][j] = energy(i, j);
-            }
-        }
+        double[][] distTo = new double[width()][height()];
+        int[][] edgeTo = new int[width()][height()];
 
-        double[][] relaxed = new double[width()][height()];
-        for (int i = 0; i < relaxed.length; i++) {
-            for (int j = 0; j < relaxed[i].length; j++) {
-                relaxed[i][j] = Double.POSITIVE_INFINITY;
-            }
-        }
-
-        double[] distTo = new double[width() * height()];
-        Stack indexesToRelax = new Stack();
-        indexesToRelax.push(0);
-
-
-        while (!indexesToRelax.isEmpty()) {
-
-        }
-
-
-        return null;
+        return findVerticalSeam(distTo, edgeTo, energy);
     }
 
     // remove horizontal seam from current picture
     public void removeHorizontalSeam(int[] seam) {
+        transposeImage();
+        removeVerticalSeam(seam);
+        transposeImage();
+        computeEnergyMatrix();
     }
 
     // remove vertical seam from current picture
     public void removeVerticalSeam(int[] seam) {
+        Picture newPicture = new Picture(width() - 1, height());
+        for (int i = 0; i < width() - 1; i++) {
+            for (int j = 0; j < height() - 1; j++) {
+                if (i >= seam[j])
+                    newPicture.set(i, j, picture.get(i + 1, j + 1));
+                else
+                    newPicture.set(i, j, picture.get(i, j));
+            }
+        }
+        this.picture = newPicture;
+        computeEnergyMatrix();
+    }
+
+    private void computeEnergyMatrix() {
+        this.energy = new double[width()][height()];
+        this.energyTransposed = new double[height()][width()];
+
+        for (int i = 0; i < energy.length; i++) {
+            for (int j = 0; j < energy[i].length; j++) {
+                energy[i][j] = energy(i, j);
+                energyTransposed[j][i] = energy[i][j];
+            }
+        }
     }
 
     private void validateColumnAndRow(int x, int y) {
@@ -99,5 +112,90 @@ public class SeamCarver {
         double bx = nextPixelColor.getBlue() - previousPixelColor.getBlue();
 
         return Math.pow(rx, 2) + Math.pow(gx, 2) + Math.pow(bx, 2);
+    }
+
+    private int[] findVerticalSeam(double[][] distTo, int[][] edgeTo, double[][] energyMatrix) {
+        init(distTo);
+        relaxAllEdgesBasedOnEnergyLevel(distTo, edgeTo, energyMatrix);
+
+        return getMinimumEnergySeam(distTo, edgeTo);
+    }
+
+    private void init(double[][] distTo) {
+        for (int i = 0; i < distTo.length; i++) {
+            for (int j = 0; j < distTo[i].length; j++) {
+                distTo[i][j] = Double.POSITIVE_INFINITY;
+            }
+        }
+    }
+
+    private void relaxAllEdgesBasedOnEnergyLevel(double[][] distTo, int[][] edgeTo, double[][] energyMatrix) {
+        final int width = distTo.length;
+        final int height = distTo[0].length;
+
+        for (int j = 0; j < width; j++) {
+            distTo[j][0] = energyMatrix[j][0];
+        }
+
+        for (int j = 0; j < height - 1; j++) {
+            for (int i = 0; i < width; i++) {
+                if ((i - 1 >= 0) && (distTo[i - 1][j + 1] > distTo[i][j] + energyMatrix[i - 1][j + 1])) {
+                    distTo[i - 1][j + 1] = distTo[i][j] + energyMatrix[i - 1][j + 1];
+                    edgeTo[i - 1][j + 1] = i;
+                }
+                if ((distTo[i][j + 1] > distTo[i][j] + energyMatrix[i][j + 1])) {
+                    distTo[i][j + 1] = distTo[i][j] + energyMatrix[i][j + 1];
+                    edgeTo[i][j + 1] = i;
+                }
+                if ((i + 1 < width) && (distTo[i + 1][j + 1] > distTo[i][j] + energyMatrix[i + 1][j + 1])) {
+                    distTo[i + 1][j + 1] = distTo[i][j] + energyMatrix[i + 1][j + 1];
+                    edgeTo[i + 1][j + 1] = i;
+                }
+            }
+        }
+    }
+
+    private int[] getMinimumEnergySeam(double[][] distTo, int[][] edgeTo) {
+        final int height = distTo[0].length;
+        int minColumn = getColumnFromLastRowWithMinimumAccumulatedEnergy(distTo);
+
+        int[] seam = new int[height];
+        seam[height - 1] = minColumn;
+
+        for (int j = height - 1; j > 0; j--) {
+            minColumn = edgeTo[minColumn][j];
+            seam[j - 1] = minColumn;
+        }
+        return seam;
+    }
+
+    private int getColumnFromLastRowWithMinimumAccumulatedEnergy(double[][] distTo) {
+        double min = Double.POSITIVE_INFINITY;
+        int minColumn = 0;
+        int width = distTo.length;
+        int height = distTo[0].length;
+
+        for (int j = 0; j < width; j++) {
+            if (min > distTo[j][height - 1]) {
+                min = distTo[j][height - 1];
+                minColumn = j;
+            }
+        }
+        return minColumn;
+    }
+
+    private void transposeImage() {
+        Picture newPicture = new Picture(height(), width());
+
+        for (int i = 0; i < picture.width(); i++) {
+            for (int j = 0; j < picture.height(); j++) {
+                newPicture.set(j, i, picture.get(i, j));
+            }
+        }
+        this.picture = newPicture;
+    }
+
+    private double energyMatrix(int row, int col) {
+        return (transposed) ? energy[col][row] : energy[row][col];
     }
 }
